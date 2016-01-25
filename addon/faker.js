6e4f03531879f9aaabd4bf984995347d6575dca6
Taylor Mock <tmock12@gmail.com>
2016-01-25 09:48:20 -0500

    Add Faker dependency

---
+++ addon/faker.js
@@ -0,0 +1,3 @@
+/* global faker */
+
+export default faker;
--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-phil'
+  name: 'ember-phil',
+
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/Faker/build/build/faker.js');
+  }
 };
