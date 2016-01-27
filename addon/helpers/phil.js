5a97ca1f2999a7dbab2f0142b993f5d2e49be0af
Taylor Mock <tmock12@gmail.com>
2016-01-27 13:29:36 -0500

    Add Phil words helper

--- addon/helpers/phil.js
+++ addon/helpers/phil.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 import iteration from '../utils/phil-iteration';
+import lorem from '../utils/phil-lorem';
 
 export function phil([property, option]) {
   switch(property) {
     case "pick":
       return iteration.pick(option);
+    case "words":
+      return lorem.words(option);
   }
   return;
 }
